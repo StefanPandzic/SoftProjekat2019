f1c1fa7b7e79eae8c99784c60db1e10fefbdd144
Stefan <fansterambo@gmail.com>
2019-02-05 00:16:58 +0100

    Dodate funkcije za kreiranje i treniranje mreze

---
+++ mreza.py
@@ -0,0 +1,38 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.utils import np_utils
+
+
+def napravi_mrezu():
+    mreza = Sequential()
+    mreza.add(Dense(512, input_shape=(784,), activation='tanh'))
+    mreza.add(Dropout(0.2))
+
+    mreza.add(Dense(512, activation='tanh'))
+    mreza.add(Dropout(0.2))
+
+    mreza.add(Dense(512, activation='tanh'))
+    mreza.add(Dropout(0.2))
+
+    mreza.add(Dense(512, activation='tanh'))
+    mreza.add(Dropout(0.2))
+
+    mreza.add(Dense(512, activation='tanh'))
+    mreza.add(Dropout(0.2))
+
+    mreza.add(Dense(10, activation='softmax'))
+
+    return mreza
+
+
+def treniraj_mrezu(mreza, X_train, y_train, X_test, y_test):
+    n_classes = 10
+    y_train = np_utils.to_categorical(y_train, n_classes)
+    y_test = np_utils.to_categorical(y_test, n_classes)
+
+    mreza.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    print('')
+    mreza.fit(X_train, y_train, epochs=10, batch_size=256, verbose=1, validation_data=(X_test, y_test))
+
+    return mreza
